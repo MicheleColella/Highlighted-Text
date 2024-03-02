d0d1932a29dcab4bbf6aa2b3c0111b7866e14753
Michele Colella <michelecolella0@gmail.com>
2024-03-02 12:56:05 +0100

    Initial Commit

---
+++ TextColorEffectTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextColorEffectTest
+//
+//  Created by Michele Colella on 02/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
